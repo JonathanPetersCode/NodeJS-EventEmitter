cac118afa578ef4e9eb9e5a0dd261b759baa55d5
Jonathan Peters <42306733+JonathanPetersCode@users.noreply.github.com>
2018-12-19 20:41:55 -0500

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,15 @@
+const EventEmitter = require('events');
+
+const url = "http://mylogger.io/log"
+
+class Logger extends EventEmitter{
+    log(message){
+        //send an htttp request
+        console.log(message);
+    
+        //Raise an event
+        this.emit("messageLogged", { id: 1, url: "http://" });
+    }
+}
+
+module.exports = Logger;
